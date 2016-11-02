c434aea9c322208f50195caf65337b2f8b3cc590
Liping <liping.sun@huawei.com>
2016-11-01 17:51:30 -0700

    Implement Account Form

--- Electronic Library/FormAccount.designer.vb
+++ Electronic Library/FormAccount.designer.vb
@@ -25,12 +25,14 @@ Partial Class AccountForm
         Me.AccountLabel = New System.Windows.Forms.Label()
         Me.ResourceLabel = New System.Windows.Forms.Label()
         Me.AccountResourceListView = New System.Windows.Forms.ListView()
-        Me.Title = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.Author = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.PublicationDate = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.Series = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.ISBN = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.Subject = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Title = CType(New System.Windows.Forms.ColumnHeader(),System.Windows.Forms.ColumnHeader)
+        Me.CheckoutDate = CType(New System.Windows.Forms.ColumnHeader(),System.Windows.Forms.ColumnHeader)
+        Me.DueDate = CType(New System.Windows.Forms.ColumnHeader(),System.Windows.Forms.ColumnHeader)
+        Me.Author = CType(New System.Windows.Forms.ColumnHeader(),System.Windows.Forms.ColumnHeader)
+        Me.PublicationDate = CType(New System.Windows.Forms.ColumnHeader(),System.Windows.Forms.ColumnHeader)
+        Me.Series = CType(New System.Windows.Forms.ColumnHeader(),System.Windows.Forms.ColumnHeader)
+        Me.ISBN = CType(New System.Windows.Forms.ColumnHeader(),System.Windows.Forms.ColumnHeader)
+        Me.Subject = CType(New System.Windows.Forms.ColumnHeader(),System.Windows.Forms.ColumnHeader)
         Me.ReturnButton = New System.Windows.Forms.Button()
         Me.RenewBotton = New System.Windows.Forms.Button()
         Me.NameLabel = New System.Windows.Forms.Label()
@@ -38,12 +40,14 @@ Partial Class AccountForm
         Me.EmailLabel = New System.Windows.Forms.Label()
         Me.DEmailLabel = New System.Windows.Forms.Label()
         Me.CloseButton = New System.Windows.Forms.Button()
-        Me.SuspendLayout()
+        Me.AddressLabel = New System.Windows.Forms.Label()
+        Me.DAddressLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout
         '
         'AccountLabel
         '
-        Me.AccountLabel.AutoSize = True
-        Me.AccountLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AccountLabel.AutoSize = true
+        Me.AccountLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 16!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
         Me.AccountLabel.Location = New System.Drawing.Point(38, 31)
         Me.AccountLabel.Name = "AccountLabel"
         Me.AccountLabel.Size = New System.Drawing.Size(91, 26)
@@ -52,8 +56,8 @@ Partial Class AccountForm
         '
         'ResourceLabel
         '
-        Me.ResourceLabel.AutoSize = True
-        Me.ResourceLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ResourceLabel.AutoSize = true
+        Me.ResourceLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 16!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
         Me.ResourceLabel.Location = New System.Drawing.Point(39, 160)
         Me.ResourceLabel.Name = "ResourceLabel"
         Me.ResourceLabel.Size = New System.Drawing.Size(105, 26)
@@ -62,18 +66,28 @@ Partial Class AccountForm
         '
         'AccountResourceListView
         '
-        Me.AccountResourceListView.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.Title, Me.Author, Me.PublicationDate, Me.Series, Me.ISBN, Me.Subject})
+        Me.AccountResourceListView.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.Title, Me.CheckoutDate, Me.DueDate, Me.Author, Me.PublicationDate, Me.Series, Me.ISBN, Me.Subject})
+        Me.AccountResourceListView.FullRowSelect = true
         Me.AccountResourceListView.Location = New System.Drawing.Point(41, 200)
         Me.AccountResourceListView.Name = "AccountResourceListView"
         Me.AccountResourceListView.Size = New System.Drawing.Size(590, 222)
         Me.AccountResourceListView.TabIndex = 2
-        Me.AccountResourceListView.UseCompatibleStateImageBehavior = False
+        Me.AccountResourceListView.UseCompatibleStateImageBehavior = false
         Me.AccountResourceListView.View = System.Windows.Forms.View.Details
         '
         'Title
         '
         Me.Title.Text = "Title"
         Me.Title.Width = 140
+        '
+        'CheckoutDate
+        '
+        Me.CheckoutDate.Text = "Checkout Date"
+        Me.CheckoutDate.Width = 87
+        '
+        'DueDate
+        '
+        Me.DueDate.Text = "Due Date"
         '
         'Author
         '
@@ -102,28 +116,28 @@ Partial Class AccountForm
         '
         'ReturnButton
         '
-        Me.ReturnButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.ReturnButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11!)
         Me.ReturnButton.Location = New System.Drawing.Point(664, 200)
         Me.ReturnButton.Name = "ReturnButton"
         Me.ReturnButton.Size = New System.Drawing.Size(90, 30)
         Me.ReturnButton.TabIndex = 3
         Me.ReturnButton.Text = "Return"
-        Me.ReturnButton.UseVisualStyleBackColor = True
+        Me.ReturnButton.UseVisualStyleBackColor = true
         '
         'RenewBotton
         '
-        Me.RenewBotton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.RenewBotton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11!)
         Me.RenewBotton.Location = New System.Drawing.Point(664, 250)
         Me.RenewBotton.Name = "RenewBotton"
         Me.RenewBotton.Size = New System.Drawing.Size(90, 30)
         Me.RenewBotton.TabIndex = 4
         Me.RenewBotton.Text = "Renew"
-        Me.RenewBotton.UseVisualStyleBackColor = True
+        Me.RenewBotton.UseVisualStyleBackColor = true
         '
         'NameLabel
         '
-        Me.NameLabel.AutoSize = True
-        Me.NameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.NameLabel.AutoSize = true
+        Me.NameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11!)
         Me.NameLabel.Location = New System.Drawing.Point(41, 77)
         Me.NameLabel.Name = "NameLabel"
         Me.NameLabel.Size = New System.Drawing.Size(48, 18)
@@ -132,18 +146,18 @@ Partial Class AccountForm
         '
         'DNameLabel
         '
-        Me.DNameLabel.AutoSize = True
-        Me.DNameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.DNameLabel.AutoSize = true
+        Me.DNameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11!)
         Me.DNameLabel.Location = New System.Drawing.Point(123, 77)
         Me.DNameLabel.Name = "DNameLabel"
         Me.DNameLabel.Size = New System.Drawing.Size(32, 18)
         Me.DNameLabel.TabIndex = 6
-        Me.DNameLabel.Text = "abc"
+        Me.DNameLabel.Text = "___"
         '
         'EmailLabel
         '
-        Me.EmailLabel.AutoSize = True
-        Me.EmailLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.EmailLabel.AutoSize = true
+        Me.EmailLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11!)
         Me.EmailLabel.Location = New System.Drawing.Point(40, 113)
         Me.EmailLabel.Name = "EmailLabel"
         Me.EmailLabel.Size = New System.Drawing.Size(45, 18)
@@ -152,30 +166,52 @@ Partial Class AccountForm
         '
         'DEmailLabel
         '
-        Me.DEmailLabel.AutoSize = True
-        Me.DEmailLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.DEmailLabel.AutoSize = true
+        Me.DEmailLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11!)
         Me.DEmailLabel.Location = New System.Drawing.Point(123, 113)
         Me.DEmailLabel.Name = "DEmailLabel"
         Me.DEmailLabel.Size = New System.Drawing.Size(32, 18)
         Me.DEmailLabel.TabIndex = 8
-        Me.DEmailLabel.Text = "bcd"
+        Me.DEmailLabel.Text = "___"
         '
         'CloseButton
         '
-        Me.CloseButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.CloseButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11!)
         Me.CloseButton.Location = New System.Drawing.Point(664, 392)
         Me.CloseButton.Name = "CloseButton"
         Me.CloseButton.Size = New System.Drawing.Size(90, 30)
         Me.CloseButton.TabIndex = 10
         Me.CloseButton.Text = "Close"
-        Me.CloseButton.UseVisualStyleBackColor = True
+        Me.CloseButton.UseVisualStyleBackColor = true
+        '
+        'AddressLabel
+        '
+        Me.AddressLabel.AutoSize = true
+        Me.AddressLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11!)
+        Me.AddressLabel.Location = New System.Drawing.Point(358, 77)
+        Me.AddressLabel.Name = "AddressLabel"
+        Me.AddressLabel.Size = New System.Drawing.Size(62, 18)
+        Me.AddressLabel.TabIndex = 11
+        Me.AddressLabel.Text = "Address"
+        '
+        'DAddressLabel
+        '
+        Me.DAddressLabel.AutoSize = true
+        Me.DAddressLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11!)
+        Me.DAddressLabel.Location = New System.Drawing.Point(358, 113)
+        Me.DAddressLabel.Name = "DAddressLabel"
+        Me.DAddressLabel.Size = New System.Drawing.Size(32, 18)
+        Me.DAddressLabel.TabIndex = 12
+        Me.DAddressLabel.Text = "___"
         '
         'AccountForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ButtonFace
         Me.ClientSize = New System.Drawing.Size(784, 462)
+        Me.Controls.Add(Me.DAddressLabel)
+        Me.Controls.Add(Me.AddressLabel)
         Me.Controls.Add(Me.CloseButton)
         Me.Controls.Add(Me.DEmailLabel)
         Me.Controls.Add(Me.EmailLabel)
@@ -186,15 +222,15 @@ Partial Class AccountForm
         Me.Controls.Add(Me.AccountResourceListView)
         Me.Controls.Add(Me.ResourceLabel)
         Me.Controls.Add(Me.AccountLabel)
-        Me.MaximizeBox = False
+        Me.MaximizeBox = false
         Me.MaximumSize = New System.Drawing.Size(800, 500)
         Me.MinimumSize = New System.Drawing.Size(800, 500)
         Me.Name = "AccountForm"
         Me.Text = "Account"
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+        Me.ResumeLayout(false)
+        Me.PerformLayout
 
-    End Sub
+End Sub
 
     Friend WithEvents AccountLabel As Label
     Friend WithEvents ResourceLabel As Label
@@ -212,4 +248,8 @@ Partial Class AccountForm
     Friend WithEvents ISBN As ColumnHeader
     Friend WithEvents Subject As ColumnHeader
     Friend WithEvents CloseButton As Button
+    Friend WithEvents AddressLabel As Label
+    Friend WithEvents DAddressLabel As Label
+    Friend WithEvents CheckoutDate As ColumnHeader
+    Friend WithEvents DueDate As ColumnHeader
 End Class
--- Electronic Library/FormAccount.vb
+++ Electronic Library/FormAccount.vb
@@ -1,10 +1,62 @@
 ﻿Public Class AccountForm
 
-    Private Sub ReturnButton_Click(sender As Object, e As EventArgs) Handles ReturnButton.Click
+    Dim db As ELibraryEntities = New ELibraryEntities()
+
+    Private Sub AccountForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim member = LoginForm.member
+        DNameLabel.Text = member.FirstName + " " + member.LastName
+        DEmailLabel.Text = member.Email
+        DAddressLabel.Text = member.StreetAddress + ", " + member.City + ", " + member.State + ", " + member.ZipCode
+
+        AccountResourceListView.Items.Clear()
+        
+        Dim checkouts = db.Checkouts.Where(Function(c) c.MemberID = member.MemberID And c.ReturnDate Is Nothing)
+        For Each c As Checkout In checkouts
+            Dim item = AccountResourceListView.Items.Add(c.Resource.Title)
+            item.Tag = c
+            item.SubItems.Add(c.CheckOutDate)
+            Dim due = CDate(c.CheckOutDate).AddDays(CInt(c.Resource.CheckOutPeriod))
+            item.SubItems.Add(due)
+        Next
 
     End Sub
 
-    Private Sub NameLabel_Click(sender As Object, e As EventArgs) Handles NameLabel.Click
+    Private Sub CloseButton_Click(sender As Object, e As EventArgs) Handles CloseButton.Click, MyBase.FormClosed
+        Me.Close()
+        SearchForm.Show()
+    End Sub
 
+    Private Sub ReturnButton_Click(sender As Object, e As EventArgs) Handles ReturnButton.Click
+        If AccountResourceListView.SelectedItems.Count = 0 Then
+            MessageBox.Show("Please select a resource first")
+            Return
+        End If
+        Dim item = AccountResourceListView.SelectedItems.Item(0)
+        item.Tag.ReturnDate = Date.Now.Date
+        db.saveChanges()
+        AccountResourceListView.Items.Remove(item)
+        MessageBox.Show("Return Succeed!")
+    End Sub
+
+    Private Sub RenewBotton_Click(sender As Object, e As EventArgs) Handles RenewBotton.Click
+        If AccountResourceListView.SelectedItems.Count = 0 Then
+            MessageBox.Show("Please select a resource first")
+            Return
+        End If
+        Dim item = AccountResourceListView.SelectedItems.Item(0)
+        item.Tag.ReturnDate = Date.Now.Date
+        Dim newCheckout As Checkout = New Checkout()
+        newCheckout.CheckoutID = db.Checkouts.Max(Function(checkout) CInt(checkout.CheckoutID)) + 1
+        newCheckout.ResourceID = item.Tag.ResourceID
+        newCheckout.MemberID = LoginForm.member.MemberID
+        newCheckout.CheckOutDate = Date.Now.Date
+        db.Checkouts.Add(newCheckout)
+        db.SaveChanges()
+        AccountResourceListView.Items.Remove(item)
+        item.Tag = newCheckout
+        item.SubItems.Item(1).Text = newCheckout.CheckOutDate
+        item.SubItems.Item(2).Text = CDate(newCheckout.CheckOutDate).AddDays(CInt(newCheckout.Resource.CheckOutPeriod))
+        AccountResourceListView.Items.Add(item)
+        MessageBox.Show("Renew Succeed!")
     End Sub
 End Class
